98cbd9f595ac0c6d7d3432a68e253d842b79a12d
Uzair Iqbal <uzair.qq@outlook.com>
2018-09-08 01:59:30 +0500

    Empty Mvc Project And Created Employee Model class with necassary Data Annotations and installed entity framework and added applicationDbContext Class

---
+++ MvcCrud/MvcCrud/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcCrud.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext() : base("MVCCrud")
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ MvcCrud/MvcCrud/Models/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcCrud.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [StringLength(200)]
+        [DisplayName("Email Id")]
+        public string EmailId { get; set; }
+
+        [StringLength(50)]
+        public string City { get; set; }
+
+        [StringLength(50)]
+        public string Country { get; set; }
+    }
+}
