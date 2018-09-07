9bb24bc0b1007002cfdba52c807a70716cc1e17d
Uzair Iqbal <uzair.qq@outlook.com>
2018-09-08 03:10:24 +0500

    Created GetEmployees Action Method and done show the Employee records in jquery table DONE GET

--- MvcCrud/MvcCrud/Controllers/HomeController.cs
+++ MvcCrud/MvcCrud/Controllers/HomeController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcCrud.Models;
 
 namespace MvcCrud.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public HomeController()
+        {
+            _context=new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetEmployees()
+        {
+            var employees = _context.Employees.OrderBy(i => i.FirstName).ToList();
+            return Json(new {data = employees}, JsonRequestBehavior.AllowGet);
         }
     }
 }
